b2d44fa26ecceca35ebdb6594320cd9e71e4ac75
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-14 19:18:22 +0700

    test react

---
+++ src/my-app/About.tsx
@@ -0,0 +1,7 @@
+const About: React.FC = () => {
+  return (
+    <div className="aaa">
+      This is About page
+    </div>
+  )
+}
--- src/my-app/App.tsx
+++ src/my-app/App.tsx
@@ -3,8 +3,15 @@ import * as React from "react";
 import ListOfNames from "./components/list-of-names";
 
 const App: React.FC = () => {
+  const inputEl = React.useRef(null)
+  const onButtonClick = () => {
+    inputEl.current.focus();
+  };
+
   return (
     <div className='aaa'>
+      <input type="text" ref={inputEl} />
+      <button onClick={onButtonClick}></button>
       <ListOfNames title={'ABC Members'} data={['Thiện', 'Luân', 'Tuyền', 'Linh', 'Hưng', 'Huy']} />
       <ListOfNames data={['Tin', 'Khánh']} />
     </div>
