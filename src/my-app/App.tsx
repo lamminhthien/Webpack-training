de8054f7cf73601636e5bb63d2e5ab88c471207a
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-15 08:26:53 +0700

    load api accordion outside component

--- src/my-app/App.tsx
+++ src/my-app/App.tsx
@@ -9,19 +9,28 @@ import BlogDetail from '@/my-app/pages/blog/detail';
 import MainMenu from '@/my-app/components/menu';
 import ToTop from '@/my-app/components/to-top';
 import Accordion from './components/accordion';
+
 import { IAcordition } from './interfaces/accordition';
 
+import * as AccorditionApi from '@/my-app/api/network/accordition';
+
+
 const App: React.FC = () => {
-  var accorditionArray: IAcordition[] = [
-    {id: 0, content: 'This is content for 0', title: 'This is title 0'},
-    {id: 1, content: 'This is content for 1', title: 'This is title 1'},
-    {id: 2, content: 'This is content for 2', title: 'This is title 2'}
-  ];
+  const [accordions, setArcordions] = React.useState<IAcordition[]>();
+  React.useEffect(() => {
+    (async () => {
+      const accordions = await AccorditionApi.find();
+      setArcordions(accordions.data);
+    })();
+  }, []);
+
+  if (!accordions) return null;
+  
   return (
     <Router>
       <MainMenu />
       <ToTop />
-      <Accordion data={accorditionArray}/>
+      <Accordion data={accordions}/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
